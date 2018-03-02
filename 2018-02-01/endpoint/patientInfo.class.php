9036db4b9542b6f6fe29874aceb2d0dcb5461dd7
Thiago Carvalho <tcarvalho@hnhad.holyname.org>
2018-03-02 15:19:40 -0500

    Updated class file

--- 2018-02-01/endpoint/patientInfo.class.php
+++ 2018-02-01/endpoint/patientInfo.class.php
@@ -5,55 +5,79 @@ class patientInfo
 {
 	private $sourceData;
 	private $processedData;
+	private $treatmentData;
+	private $paymentData = [];
 
 	// Sets the object to the specific value
-	public function setSourceData($information) {
-		$this->sourceData = $information;
+	public function setSourceData($passedSourceData)
+	{
+		$this->sourceData = $passedSourceData;
 	}
 
 	// Returns the (new) value of the object
-	public function getSourceData() {
+	public function getSourceData()
+	{
     	return $this->sourceData;
+    }
+
+    // Organizes the source data
+    public function doProcessData($passedSourceData)
+    {
+    	// Loops through the given array
+    	foreach ($passedSourceData as $patientInfo) {
+    		$patientArray['Patient']['id'] = $patientInfo['patientId'];
+			$patientArray['Patient']['firstName'] = $patientInfo['firstName'];
+			$patientArray['Patient']['lastName'] = $patientInfo['lastName'];
+
+			$patientArray['Patient']['Treatments'][$patientInfo['treatmentName']]['treatmentName'] = $patientInfo['treatmentName'];
+			$patientArray['Patient']['Treatments'][$patientInfo['treatmentName']]['treatmentCost'] = $patientInfo['treatmentCost'];
+			$patientArray['Patient']['Treatments'][$patientInfo['treatmentName']]['treatmentDate'] = $patientInfo['treatmentDate'];
+
+			$patientArray['Patient']['Treatments'][$patientInfo['treatmentName']]['Payments'][] =
+				[
+					'paymentId' => $patientInfo['paymentId'],
+					'paymentDate' => $patientInfo['paymentDate'],
+					'amount' => $patientInfo['paymentAmount']
+				];
+
+			// Saves all Payment-related data
+			// Must stay within the loop
+			$this->paymentData[$patientInfo['treatmentName']] = $patientArray['Patient']['Treatments'][$patientInfo['treatmentName']]['Payments'];
+
+    	}// End of Loop
+
+    	$this->processedData = $patientArray;
+    	$this->treatmentData = $patientArray['Patient']['Treatments'];
+    }
+
+    // Returns the organized data
+    public function getProcessedData()
+    {
+    	return $this->processedData;
+    }
+
+    public function getTreatmentData()
+    {
+    	return $this->treatmentData;
+    }
+
+    public function getPaymentData()
+    {
+    	return $this->paymentData;
     }
 }
 
-$patientArray = new patientInfo();
-$patientArray->setSourceData($UnknownTable);
-$patientArray->getSourceData();
-// $foo->setBar($foo->getBar() + 1);
+// Sets up a new OBJECT
+$newPatient = new patientInfo();
+
+$newPatient->setSourceData($UnknownTable);
+$newPatient->getSourceData();
+
+$newPatient->doProcessData($UnknownTable);
+$newPatient->getProcessedData();
+
+$newPatient->getTreatmentData();
+$newPatient->getPaymentData();
 
 echo "<pre>";
-print_r($patientArray);
-
-
-// class patientInfo
-// {
-// 	private $sourceData;
-// 	private $processedData;
-
-// 	// $attributes is the array holding the patient information
-// 	public function __construct($information = array()) {
-// 		foreach ($information as $key => $value) {
-// 			$this->$key = $value;
-// 		}
-// 	}
-
-// 	function setSourceData($name, $value) {
-// 		if(method_exists($this, $name)) {
-// 			$this->$name($value);
-// 		}
-// 		else {
-// 			$this->$name = $value;
-// 		}
-// 	}
-
-// 	function getSourceData($name) {
-// 		if(method_exists($this, $name)) {
-// 			return $this->$name();
-// 		}
-// 		elseif(property_exists($this, $name)) {
-// 			return $this->name;
-// 		}
-// 		return null;
-// 	}
-// }
+print_r($newPatient);
